2d372f04b72b7cf2f18674c8a103386f84ce0356
Artemiy Laptev <artemiy.laptev@gmail.com>
2023-03-21 12:18:17 +0300

    21.03 12:18 commit

--- src/main/java/com/javarush/cryptanalyser/laptev/model/CryptoanizerCaesar.java
+++ src/main/java/com/javarush/cryptanalyser/laptev/model/CryptoanizerCaesar.java
@@ -16,90 +16,92 @@ public class CryptoanizerCaesar {
             "-g  - Запустить графический интерфейс.";
 
     public static void main(String[] args) {
+
         ICryptoanizerCaesarConsole iccConsole = new CryptoanizerCaesarConsole();
         ICryptoanizerCaesarGUI iccGUI = new CryptoanizerCaesarGUI(iccConsole);
-        if (args.length == 0) {
-            System.err.println(errorMessage);
-            System.exit(1);
-        }
-        String progKey = args[0];
-        if (progKey.equals("-c")) {
-            if (args.length != 2) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-            String file = args[1];
-            iccConsole.readFileToSourceTxt(file);
-            iccConsole.saveTxtToFile("copy.txt", iccConsole.getSourceTxt());
-        } else if (progKey.equals("-e")) {
-            if (args.length != 3) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-            try {
-                int encryptKey = Integer.parseInt(args[1]);
-                if (encryptKey >= iccConsole.getAlphabet().length()) {
-                    System.err.println(errorMessage);
-                    System.exit(1);
-                }
-                String file = args[2];
-                iccConsole.readFileToSourceTxt(file);
-                iccConsole.encryptTxt(iccConsole.getSourceTxt(), encryptKey);
-                iccConsole.saveTxtToFile("encoded.txt", iccConsole.getEncodeTxt());
-            } catch (NumberFormatException e) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-        } else if (progKey.equals("-d")) {
-            if (args.length != 3) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-            try {
-                int encryptKey = Integer.parseInt(args[1]);
-                if (encryptKey >= iccConsole.getAlphabet().length()) {
-                    System.err.println(errorMessage);
-                    System.exit(1);
-                }
-                String file = args[2];
-                iccConsole.readFileToSourceTxt(file);
-                iccConsole.encryptTxt(iccConsole.getSourceTxt(), -Math.abs(encryptKey));
-                iccConsole.saveTxtToFile("output.txt", iccConsole.getEncodeTxt());
-            } catch (NumberFormatException e) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-        } else if (progKey.equals("-b")) {
-            if (args.length != 2) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-            String file = args[1];
-            iccConsole.readFileToSourceTxt(file);
-            int key = iccConsole.bruteForceSourceTxt();
-            iccConsole.encryptTxt(iccConsole.getSourceTxt(), key);
-            iccConsole.saveTxtToFile("output.txt", iccConsole.getEncodeTxt());
-        } else if (progKey.equals("-s")) {
-            if (args.length != 3) {
-                System.err.println(errorMessage);
-                System.exit(1);
-            }
-            String cryptFile = args[1];
-            String planeFile = args[2];
-            iccConsole.readFileToSourceTxt(planeFile);
-            iccConsole.setReferenceTxt(iccConsole.getSourceTxt());
-            iccConsole.readFileToSourceTxt(cryptFile);
-            iccConsole.setEncodeTxt(iccConsole.getSourceTxt());
-            int key = iccConsole.statAnalizSourceTxtByReferenceTxt(iccConsole.getEncodeTxt(), iccConsole.getReferenceTxt());
-            iccConsole.encryptTxt(iccConsole.getSourceTxt(), -key);
-            iccConsole.saveTxtToFile("output.txt", iccConsole.getEncodeTxt());
-        } else if (progKey.equals("-g")) {
-            iccGUI.mainWindow();
-            iccConsole.setGuiMark(true);
-        } else if (progKey.equals("-help")) {
-            System.out.println(helpMessage);
-        } else {
-            System.out.println(helpMessage);
-        }
+        iccGUI.mainWindow();
+//        if (args.length == 0) {
+//            System.err.println(errorMessage);
+//            System.exit(1);
+//        }
+//        String progKey = args[0];
+//        if (progKey.equals("-c")) {
+//            if (args.length != 2) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//            String file = args[1];
+//            iccConsole.readFileToSourceTxt(file);
+//            iccConsole.saveTxtToFile("copy.txt", iccConsole.getSourceTxt());
+//        } else if (progKey.equals("-e")) {
+//            if (args.length != 3) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//            try {
+//                int encryptKey = Integer.parseInt(args[1]);
+//                if (encryptKey >= iccConsole.getAlphabet().length()) {
+//                    System.err.println(errorMessage);
+//                    System.exit(1);
+//                }
+//                String file = args[2];
+//                iccConsole.readFileToSourceTxt(file);
+//                iccConsole.encryptTxt(iccConsole.getSourceTxt(), encryptKey);
+//                iccConsole.saveTxtToFile("encoded.txt", iccConsole.getEncodeTxt());
+//            } catch (NumberFormatException e) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//        } else if (progKey.equals("-d")) {
+//            if (args.length != 3) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//            try {
+//                int encryptKey = Integer.parseInt(args[1]);
+//                if (encryptKey >= iccConsole.getAlphabet().length()) {
+//                    System.err.println(errorMessage);
+//                    System.exit(1);
+//                }
+//                String file = args[2];
+//                iccConsole.readFileToSourceTxt(file);
+//                iccConsole.encryptTxt(iccConsole.getSourceTxt(), -Math.abs(encryptKey));
+//                iccConsole.saveTxtToFile("output.txt", iccConsole.getEncodeTxt());
+//            } catch (NumberFormatException e) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//        } else if (progKey.equals("-b")) {
+//            if (args.length != 2) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//            String file = args[1];
+//            iccConsole.readFileToSourceTxt(file);
+//            int key = iccConsole.bruteForceSourceTxt();
+//            iccConsole.encryptTxt(iccConsole.getSourceTxt(), key);
+//            iccConsole.saveTxtToFile("output.txt", iccConsole.getEncodeTxt());
+//        } else if (progKey.equals("-s")) {
+//            if (args.length != 3) {
+//                System.err.println(errorMessage);
+//                System.exit(1);
+//            }
+//            String cryptFile = args[1];
+//            String planeFile = args[2];
+//            iccConsole.readFileToSourceTxt(planeFile);
+//            iccConsole.setReferenceTxt(iccConsole.getSourceTxt());
+//            iccConsole.readFileToSourceTxt(cryptFile);
+//            iccConsole.setEncodeTxt(iccConsole.getSourceTxt());
+//            int key = iccConsole.statAnalizSourceTxtByReferenceTxt(iccConsole.getEncodeTxt(), iccConsole.getReferenceTxt());
+//            iccConsole.encryptTxt(iccConsole.getSourceTxt(), -key);
+//            iccConsole.saveTxtToFile("output.txt", iccConsole.getEncodeTxt());
+//        } else if (progKey.equals("-g")) {
+//            iccGUI.mainWindow();
+//            iccConsole.setGuiMark(true);
+//        } else if (progKey.equals("-help")) {
+//            System.out.println(helpMessage);
+//        } else {
+//            System.out.println(helpMessage);
+//        }
     }
 }
